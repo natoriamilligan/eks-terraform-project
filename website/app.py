e9eb358dfeba1d44d46fd6aae1447229bcea178f
Natoria Milligan <natoriaray@gmail.com>
2025-12-29 15:47:27 -0600

    added all files for web app

---
+++ website/app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api')
+def get_message():
+    return jsonify({"message": "Hello from the backend!"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)

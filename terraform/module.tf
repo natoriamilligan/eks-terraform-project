02493099d34371df794ac8693a67e4d169d182de
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-01-05 11:31:32 -0600

    added eks module

---
+++ terraform/module.tf
@@ -0,0 +1,20 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = ">= 21.10.1"  
+
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.35.0"
+  subnets         = local.private_subnet_ids
+  vpc_id          = aws_vpc.main.id
+
+  node_groups = {
+    default = {
+      desired_capacity = 1
+      max_capacity     = 1
+      min_capacity     = 1
+      instance_type    = "t3.small"
+    }
+  }
+
+  manage_aws_auth = true  # lets module manage cluster RBAC
+}

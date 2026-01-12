e3fd3b7bae2aa60892f996b35bf6f13f53cd5863
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-01-12 16:58:13 -0600

    created data block to access account ID and

---
+++ terraform/bootstrap/data.tf
@@ -0,0 +1,16 @@
+# Create assume role policy for lambda
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+# Provides data about the current authenticated AWS identity
+data "aws_caller_identity" "current" {}

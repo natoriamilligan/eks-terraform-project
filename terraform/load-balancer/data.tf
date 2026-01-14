5666de7e5285b74a59b431ea9324a44d9e07b135
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-01-14 17:00:52 -0600

    added trust policy for lb controller

---
+++ terraform/load-balancer/data.tf
@@ -0,0 +1,27 @@
+data "aws_iam_policy_document" "lb_controller_trust_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.eks.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub"
+      values   = [
+        "system:serviceaccount:kube-system:aws-load-balancer-controller"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+  }
+}
+}

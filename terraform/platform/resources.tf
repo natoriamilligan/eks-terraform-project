42e4335b91651e31295098a2594243ca661fbb69
Natoria Milligan <natoriaray@gmail.com>
2026-01-14 14:02:37 -0600

    removed vpc and netowrking from bootrap file and added to platform file

--- terraform/platform/resources.tf
+++ terraform/platform/resources.tf
@@ -4,6 +4,95 @@ locals {
   s3_origin_id = "s3origin"
 }
 
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create public subnets
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+# Create public route table and connect internet gateway
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Connect public subnet to route table for internet access
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+# Create elastic IP for NAT Gateway
+resource "aws_eip" "ngw" {}
+
+# Create NAT Gateway
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw.id
+  subnet_id     = aws_subnet.public_a.id
+
+  depends_on    = [aws_internet_gateway.igw]
+}
+
+# Create private route table and connect NAT gateway
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_nat_gateway.ngw.id
+  }
+}
+
+# Create private subnets
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+}
+
+# Attach private subnets to private route tables
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 # Create S3 bucket
 resource "aws_s3_bucket" "app_bucket" {
   bucket = local.root_domain

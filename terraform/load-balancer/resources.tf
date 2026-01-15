26a19de63edb65146905fb545080b5a9937e059d
Natoria Milligan <natoriaray@gmail.com>
2026-01-15 11:31:05 -0600

    changed name for assume policy

--- terraform/load-balancer/resources.tf
+++ terraform/load-balancer/resources.tf
@@ -12,7 +12,7 @@ terraform {
 # Create Load Balancer Controller IAM role
 resource "aws_iam_role" "lb_controller_role" {
   name               = "lb-controller-role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+  assume_role_policy = data.aws_iam_policy_document.lb_controller_assume_policy.json
 }
 
 # Create policy for LB Controller

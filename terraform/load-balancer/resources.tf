1c1ea2c631ec65285c77e53eda9576ab76f20076
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-01-16 14:48:15 -0600

    added lb controller via helm chart

--- terraform/load-balancer/resources.tf
+++ terraform/load-balancer/resources.tf
@@ -37,3 +37,38 @@ resource "kubernetes_service_account" "lb_controller" {
     }
   }
 }
+
+# Install LB Controller from Helm LB chart
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  version    = "2.17.1"
+
+  set {
+    name  = "clusterName"
+    value = data.terraform_remote_state.eks.outputs.cluster_name
+  }
+
+  set {
+    name  = "region"
+    value = var.region
+  }
+
+  set {
+    name  = "vpcId"
+    value = data.terraform_remote_state.eks.outputs.vpc_id
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = kubernetes_service_account.lb_controller.name
+  }
+}

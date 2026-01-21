df34647d6b7da64091fd40ec5816402a930d572f
Natoria Milligan <natoriaray@gmail.com>
2026-01-21 11:14:44 -0600

    added providers to terraform

--- terraform/core-infra/providers.tf
+++ terraform/core-infra/providers.tf
@@ -1,3 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+    
+    random = {
+      source  = "hashicorp/random"
+      version = "3.8.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
+
+provider "random" {}
+
--- terraform/load-balancer/providers.tf
+++ terraform/load-balancer/providers.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }

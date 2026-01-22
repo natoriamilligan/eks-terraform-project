d89c835940e161a5ff508446e1e2e862ad475e7a
Natoria Milligan <natoriaray@gmail.com>
2026-01-22 15:30:36 -0600

    added prometheus/grafana and service monitor

--- terraform/monitoring/resources.tf
+++ terraform/monitoring/resources.tf
@@ -8,3 +8,65 @@ terraform {
     encrypt        = true
   }
 }
+
+resource "helm_release" "monitoring" {
+  name       = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+
+  namespace         = "monitoring"
+  create_namespace = true
+
+  values = [
+    yamlencode({
+      grafana = {
+        enabled = true
+
+        adminUser     = "admin"
+        adminPassword = "changeme"
+
+        service = {
+          type = "ClusterIP"
+        }
+      }
+
+      prometheus = {
+        prometheusSpec = {
+          retention = "7d"
+        }
+      }
+    })
+  ]
+}
+
+resource "kubernetes_manifest" "service_monitor" {
+  manifest = {
+    apiVersion = "monitoring.coreos.com/v1"
+    kind       = "ServiceMonitor"
+
+    metadata = {
+      name      = "app-service-monitor"
+      namespace = "monitoring"
+    }
+
+    spec = {
+      selector = {
+        matchLabels = {
+          app = "app-pod"
+        }
+      }
+
+      namespaceSelector = {
+        matchNames = ["app"]
+      }
+
+      endpoints = [
+        {
+          port = "http"
+          path = "/metrics"
+          interval = "30s"
+        }
+      ]
+    }
+  }
+}

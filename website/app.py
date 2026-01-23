1c9875dbb80eb147ec2fa728ade5d61edb4a6a5f
Natoria Milligan <46578731+natoriamilligan@users.noreply.github.com>
2026-01-23 14:26:09 -0600

    updated username variable

--- website/app.py
+++ website/app.py
@@ -11,7 +11,7 @@ def get_connection():
     return psycopg2.connect(
         host=os.environ["DB_HOST"],
         database=os.environ["DB_NAME"],
-        user=os.environ["DB_USER"],
+        user=os.environ["DB_USERNAME"],
         password=os.environ["DB_PASSWORD"],
         cursor_factory=RealDictCursor
     )
